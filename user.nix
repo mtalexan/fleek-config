2c5040a2d68222cf2bb34c8c169b363bc518828a
Mike <github@trackit.fe80.email>
2023-07-15 00:20:22 -0400

    fleek: apply

--- user.nix
+++ user.nix
@@ -7,8 +7,8 @@
   ];
 
   sessionVariables = {
-    GCC_COLORS = 'error=01;31;warning=01;35:note=01;36:caret=01;32:locus=01:quote=01';
-    PAGER = 'less -R';
+    GCC_COLORS = "error=01;31;warning=01;35:note=01;36:caret=01;32:locus=01:quote=01";
+    PAGER = "less -R";
   };
 
   programs.starship = {
