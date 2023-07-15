b09145fb92e277419909cbc163f5a5471f11daa4
Mike <github@trackit.fe80.email>
2023-07-15 12:05:38 -0400

    fleek: apply

--- programs.nix
+++ programs.nix
@@ -8,6 +8,7 @@
 
   # User specified programs 
     programs.dircolors.enable = true; 
-    programs.starship.enable = true;
+    programs.starship.enable = true; 
+    programs.atuin.enable = true;
 
 }
