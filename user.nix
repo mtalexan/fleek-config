039f04bda08da84f45b6f21d98ef3185d9e1f502
Mike <github@trackit.fe80.email>
2023-07-15 12:29:34 -0400

    fleek: apply

--- user.nix
+++ user.nix
@@ -707,7 +707,6 @@
   home = {
     sessionVariables = {
       GCC_COLORS = "error=01;31;warning=01;35:note=01;36:caret=01;32:locus=01:quote=01";
-      PAGER = "less -R";
       SUDOEDITOR = "nvim";
       GIT_EDITOR = "nvim";
     };
