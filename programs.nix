77049c539f30a10d128278fae050fca9173041d0
Mike <github@trackit.fe80.email>
2023-07-16 23:27:08 -0400

    fleek: apply

--- programs.nix
+++ programs.nix
@@ -11,6 +11,7 @@
     programs.starship.enable = true; 
     programs.atuin.enable = true; 
     programs.exa.enable = true; 
-    programs.bat.enable = true;
+    programs.bat.enable = true; 
+    programs.fzf.enable = true;
 
 }
