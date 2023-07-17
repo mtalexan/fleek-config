200d4bfef1231bd56ecf6aae2983433bb9d0f46b
Mike <github@trackit.fe80.email>
2023-07-16 23:37:34 -0400

    fleek: apply

--- user.nix
+++ user.nix
@@ -56,6 +56,7 @@
       "--bind 'ctrl-/:change-preview-window(right|hidden|)'"
       "--scheme=path"
       "--filepath-word"
+      "--multi"
     ];
     # Ctrl+T command
     fileWidgetCommand = "fd --type f --hidden --follow --exclude \".git\" .";
@@ -65,6 +66,7 @@
       "--bind 'ctrl-/:change-preview-window(right|hidden|)'"
       "--scheme=path"
       "--filepath-word"
+      "--multi"
     ];
   };
 
