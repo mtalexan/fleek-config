e129bd491a548f2bbc930e2678a56d510a03846a
Mike <github@trackit.fe80.email>
2023-07-28 15:33:50 -0400

    fleek: apply

--- user.nix
+++ user.nix
@@ -828,7 +828,6 @@
       GCC_COLORS = "error=01;31;warning=01;35:note=01;36:caret=01;32:locus=01:quote=01";
       SUDOEDITOR = "nvim";
       GIT_EDITOR = "nvim";
-      PAGER = "bat";
     };
   };
 
