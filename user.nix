05196e4a4aeb9e7e1f634bebf83a8263db5b5eba
Mike <github@trackit.fe80.email>
2023-07-28 16:49:13 -0400

    fleek: apply

--- user.nix
+++ user.nix
@@ -198,6 +198,17 @@
     enableZshIntegration = true;
   };
 
+  # notifies of completed commands. Can also Ctrl+Z a command and then add noti to it with:
+  #   fg; noti
+  # Always includes banner notifications, but can include Telegram, Slack, Zulip, whatever
+  # if configured.
+  programs.noti = {
+    enable = true;
+    # settings = {
+    #  # see   https://github.com/variadico/noti/blob/main/docs/noti.md#configuration
+    #};
+  };
+
   ####################################################################################
   # prompt
 
