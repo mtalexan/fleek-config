f77a60a2df3edfe9687e9942bfd6cab102816e50
Alexander, Michael <github@trackit.fe80.email>
2023-08-01 12:46:03 -0400

    fleek: apply

--- goln-5cl17g3/custom.nix
+++ goln-5cl17g3/custom.nix
@@ -1,5 +1,31 @@
 { pkgs, misc, ... }: {
   # FEEL FREE TO EDIT: This file is NOT managed by fleek. 
 
- 
+  home.sessionPath = home.sessionPath ++ [
+    "$HOME/DevTools/bin"
+  ];
+
+  programs.bash.initExtra += ''
+    
+    if [ -e "$HOME/.cargo/env" ] ; then
+      source $HOME/.cargo/env
+    fi
+    if [ -e "$HOME/DevTools/.bashrc" ] ; then
+      source $HOME/DevTools/.bashrc
+    fi
+    if [ -e "$HOME/.jfrog/jfrog_bash_completion" ] ; then
+      source $HOME/.jfrog/jfrog_bash_completion
+    fi
+  '';
+  programs.zsh.initExtra += ''
+
+    if [ -e "$HOME/.cargo/env" ] ; then
+      source $HOME/.cargo/env
+    fi
+    if [ -e "$HOME/.jfrog/jfrog_bash_completion" ] ; then
+      source $HOME/.jfrog/jfrog_bash_completion
+    fi
+  '';
 }
+
+# vim: sw=2;expandtab
