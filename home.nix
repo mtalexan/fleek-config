8c52c378a4fe6a264ce60f869a7e352994a72d87
Alexander, Michael <github@trackit.fe80.email>
2023-08-01 13:06:34 -0400

    remove packages: getoptions

--- home.nix
+++ home.nix
@@ -49,7 +49,6 @@
     pkgs.zsh-fast-syntax-highlighting
     pkgs.bash-completion
     pkgs.nix-bash-completions
-    pkgs.getoptions
     # Fleek Bling
     pkgs.git
     (pkgs.nerdfonts.override { fonts = [ "FiraCode" ]; })
