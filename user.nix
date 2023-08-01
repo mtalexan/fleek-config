521010179cd1cb7a7847e3defd98267299d82612
Alexander, Michael <github@trackit.fe80.email>
2023-08-01 13:34:07 -0400

    fleek: apply

--- user.nix
+++ user.nix
@@ -928,7 +928,6 @@
       zstyle ':completion:*' completer _list _expand _complete _ignored _match
       zstyle ':completion:*' completions 1
       zstyle ':completion:*' insert-unambiguous true
-      zstyle ':completion:*' list-colors $${(s.:.)LS_COLORS}
       zstyle ':completion:*' preserve-prefix '//[^/]##/'
       zstyle ':completion:*' use-cache yes
     '';
