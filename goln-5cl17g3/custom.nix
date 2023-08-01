191f8a701052e4bdd02dcc122c2f3bf1df99bcf7
Alexander, Michael <github@trackit.fe80.email>
2023-08-01 12:49:42 -0400

    fleek: apply

--- goln-5cl17g3/custom.nix
+++ goln-5cl17g3/custom.nix
@@ -1,7 +1,7 @@
 { pkgs, misc, ... }: {
   # FEEL FREE TO EDIT: This file is NOT managed by fleek. 
 
-  home.sessionPath = home.sessionPath ++ [
+  home.sessionPath = [
     "$HOME/DevTools/bin"
   ];
 
