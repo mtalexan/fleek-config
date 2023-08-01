1c3897ed3110ed6d61a0b15ec0ff9a7d409a21e7
Alexander, Michael <github@trackit.fe80.email>
2023-08-01 12:49:17 -0400

    fleek: apply

--- goln-5cl17g3/custom.nix
+++ goln-5cl17g3/custom.nix
@@ -17,7 +17,7 @@
       source $HOME/.jfrog/jfrog_bash_completion
     fi
   '';
-  programs.zsh.initExtra = programs.zsh.initExtra + ''
+  programs.zsh.initExtra = ''
 
     if [ -e "$HOME/.cargo/env" ] ; then
       source $HOME/.cargo/env
