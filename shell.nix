df7705d3d4608f091bb38399af29265fe92d6954
Alexander, Michael <github@trackit.fe80.email>
2023-08-01 15:50:04 -0400

    fleek: apply

--- shell.nix
+++ shell.nix
@@ -1,13 +1,6 @@
 { pkgs, misc, ... }: {
-  # DO NOT EDIT: This file is managed by fleek. Manual changes will be overwritten.# bash
-  programs.bash.profileExtra = "[ -r ~/.nix-profile/etc/profile.d/nix.sh ] && source  ~/.nix-profile/etc/profile.d/nix.sh";
-  programs.bash.initExtra = ''
-    if [ -f /etc/bashrc ]; then
-        . /etc/bashrc
-    fi
-    source <(fleek completion bash)
-  '';
-  programs.bash.enableCompletion = true;
-  programs.bash.enableVteIntegration = true;
-  programs.bash.enable = true;
+  # DO NOT EDIT: This file is managed by fleek. Manual changes will be overwritten.# zsh
+  programs.zsh.profileExtra = "[ -r ~/.nix-profile/etc/profile.d/nix.sh ] && source  ~/.nix-profile/etc/profile.d/nix.sh";
+  programs.zsh.enableCompletion = true;
+  programs.zsh.enable = true;
 }
