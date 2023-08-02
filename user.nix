0907b6a2507f8d2aab8ba827fbad7c27e2d70f9f
Alexander, Michael <github@trackit.fe80.email>
2023-08-02 14:38:24 -0400

    fleek: apply

--- user.nix
+++ user.nix
@@ -276,8 +276,10 @@ in
   programs.script-directory = {
     enable = true;
     settings = {
-      # within the home-manager config folder, ~/.local/share/fleek/sd_scripts
-      SD_ROOT = "${home.homeDirectory}/sd_scripts";
+      # within the home-manager config folder, ~/.local/share/fleek/sd_scripts.
+      # This makes the folder and all files in it part of the nix package automatically, and 
+      # uses a path relative to the home-manager root file (flake.nix)
+      SD_ROOT = "${./sd_scripts}";
       # defaults to EDITOR or VISUALEDITOR if not set
       SD_EDITOR = "nvim";
       # defaults to 'cat' if not set
