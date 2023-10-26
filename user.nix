215adc0d62147014620dc797700dec31688bf47c
Alexander, Michael <github@trackit.fe80.email>
2023-10-26 18:44:18 -0400

    fleek: apply

--- user.nix
+++ user.nix
@@ -167,10 +167,16 @@ in
       # and the interactive.diffFilter
       enable = true;
       options = {
+        # these options have to be put under a separate feature name so they don't get applied
+        # when using 'git add -i' or 'git add -p', that requires a specific text input format.
+        decorations = {
+          side-by-side = true;
+          # files in listings need a box
+          file-style = "bold yellow box";
+        };
+        features = "decorations";
         # themese are set to mirror bat automatically
         line-numbers = true;
-        side-by-side = false;
-        #keep-plus-minus-markers = true;
         navigate = true;
         hyperlinks = true;
         # this causes it to open in vscode when hyperlinks to commits are clicked
@@ -181,8 +187,6 @@ in
         tabs = 4;
         # header blocks only extend to the end of the file section and not the entire terminal width
         width = "variable";
-        # files in listings need a box
-        #file-style = "bold yellow box";
       };
     };
     extraConfig = {
