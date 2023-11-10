8b96976bd32e52ed6d3f84e45d2638abaeda9d50
Alexander, Michael <michael.alexander@karlstorz.com>
2023-11-09 16:49:53 -0800

    fleek: apply

--- goln-5cl17g3/custom.nix
+++ goln-5cl17g3/custom.nix
@@ -1,9 +1,45 @@
 { pkgs, misc, ... }: {
   # FEEL FREE TO EDIT: This file is NOT managed by fleek. 
 
-  # configured in the main file, selected to be enabled for this host
-  #home.file.distrobox_config.enable = true;
-  home.file.distrobox_hooks.enable = true;
+  #####################################
+  # Files (arbitrary)
+  #####################################
+
+  # The primary distrobox config file
+  home.file.distrobox_config = {
+    enable = true;
+    executable = false;
+    # target, relative to HOME
+    target = ".config/distrobox/distrobox.conf";
+    text = [
+      # support the init hooks (see home.file.distrobox_preinithooks)
+      """container_pre_init_hook="~/.config/distrobox/pre-init-hooks.sh""""
+      # support the init hooks (see home.file.distrobox_inithooks)
+      """container_init_hook="~/.config/distrobox/init-hooks.sh""""
+      # configure it to use docker
+      """container_manager="docker""""
+    ];
+  };
+
+  # distrobox hooks to copy are host-name specific
+  home.file.distrobox_hooks = {
+    enable = true;
+    # keep the permissions from the files in the fleek folder
+    executable = null;
+    # Make each individual file a symlink in the copy rather than symlinking the
+    # whole directory. We put home.file.distrobox_config here too, so we can't do the
+    # latter.
+    recursive = true;
+
+    target = ".config/distrobox"
+    # relative to the repo root, under the host-specific folder
+    #source = "goln-5cl17g3/home_files/distrobox";
+    source = "${hostname}/home_files/distrobox";
+  };
+
+  #####################################
+  # Programs
+  #####################################
 
   home.sessionPath = [
     "$HOME/DevTools/bin"
