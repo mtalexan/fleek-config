04a4372a2b39c4d76ad67ad21f6f5f3369d69d1a
Alexander, Michael <michael.alexander@karlstorz.com>
2023-11-09 16:54:46 -0800

    fleek: apply

--- goln-5cl17g3/custom.nix
+++ goln-5cl17g3/custom.nix
@@ -5,35 +5,33 @@
   # Files (arbitrary)
   #####################################
 
-  # The primary distrobox config file
-  home.file.distrobox_config = {
-    enable = false;
-    executable = false;
-    # target, relative to HOME
-    target = ".config/distrobox/distrobox.conf";
-    text = [
-      # support the init hooks (see home.file.distrobox_preinithooks)
-      """container_pre_init_hook="~/.config/distrobox/pre-init-hooks.sh""""
-      # support the init hooks (see home.file.distrobox_inithooks)
-      """container_init_hook="~/.config/distrobox/init-hooks.sh""""
-      # configure it to use docker
-      """container_manager="docker""""
-    ];
-  };
+  home.file = {
+    # The primary distrobox config file
+    ".config/distrobox/distrobox.conf" = {
+      enable = false;
+      executable = false;
+      text = [
+        # support the init hooks (see home.file.distrobox_preinithooks)
+        """container_pre_init_hook="~/.config/distrobox/pre-init-hooks.sh""""
+        # support the init hooks (see home.file.distrobox_inithooks)
+        """container_init_hook="~/.config/distrobox/init-hooks.sh""""
+        # configure it to use docker
+        """container_manager="docker""""
+      ];
+    };
 
-  # distrobox hooks to copy are host-name specific
-  home.file.distrobox_hooks = {
-    enable = true;
-    # keep the permissions from the files in the fleek folder
-    executable = null;
-    # Make each individual file a symlink in the copy rather than symlinking the
-    # whole directory. We put home.file.distrobox_config here too, so we can't do the
-    # latter.
-    recursive = true;
-
-    target = ".config/distrobox"
-    # relative to the repo root, under the host-specific folder
-    source = "home_files/distrobox";
+    # distrobox hooks to copy are host-name specific
+    ".config/distrobox" = {
+      enable = true;
+      # keep the permissions from the files in the fleek folder
+      executable = null;
+      # Make each individual file a symlink in the copy rather than symlinking the
+      # whole directory. We put home.file.distrobox_config here too, so we can't do the
+      # latter.
+      recursive = true;
+      # relative to the repo root, under the host-specific folder
+      source = "home_files/distrobox";
+    };
   };
 
   #####################################
