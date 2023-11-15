a76d7bccdbe3f9f8e632d7c8f7752ce0a39d1692
Alexander, Michael <michael.alexander@karlstorz.com>
2023-11-15 16:31:53 -0500

    fleek: apply

--- user.nix
+++ user.nix
@@ -20,107 +20,110 @@ in
   # Each of the following should define an option.custom.files.X.enable, then set the
   # matching home.file.X.enable with the option value.
 
-  options.custom.files.".config/distrobox/distrobox.conf".enable = lib.mkEnableOption(lib.mdDoc "distrobox.conf file auto-population");
-  config.home.file.".config/distrobox/distrobox.conf" = {
-    enable = config.custom.files.".config/distrobox/distrobox.conf".enable;
-    executable = false;
-    text =
-      ''
-        # support the init hooks (see home.file.distrobox_preinithooks)
-        container_pre_init_hook="~/.config/distrobox/pre-init-hooks.sh"
-        # support the init hooks (see home.file.distrobox_inithooks)
-        container_init_hook="~/.config/distrobox/init-hooks.sh"
-      '';
-  };
-
-  # distrobox hooks to copy are host-name specific
-  options.custom.files.".config/distrobox".enable = lib.mkEnableOption(lib.mdDoc "distrobox pre-init and init hooks");
-  config.home.file.".config/distrobox" = {
-    # 'source' must be set in the custom.nix!
-    #source = ./home_files/distrobox;
-
-    enable = config.custom.files.".config/distrobox".enable;
-    # keep the permissions from the files in the fleek folder
-    executable = null;
-    # Make each individual file a symlink in the copy rather than symlinking the
-    # whole directory. We put other home.file's here too, so we can't do the
-    # latter.
-    recursive = true;
-  };
-
-  # The basic settings for podman.
-  # Still requires uidmap to be installed manually from built-in package manager.
-  # Pulled from Ubuntu.
-  options.custom.files.".config/containers".enable = lib.mkEnableOption(lib.mdDoc "podman ubuntu-like registry and storage settings");
-  config.home.file.".config/containers" = {
-    enable = config.custom.files.".config/containers".enable;
-    executable = false;
-    # Make each individual file a symlink in the copy rather than symlinking the
-    # whole directory. We put other home.file's here too, so we can't do the
-    # latter.
-    recursive = true;
-    source = ./home_files/podman_config;
-  };
-
-  # set of pre-defined short name aliases for images via podman
-  options.custom.files.".config/containers/registries.conf.d/000-shortnames.conf".enable = lib.mkEnableOption(lib.mdDoc "podman predefined list of public image aliases");
-  config.home.file.".config/containers/registries.conf.d/000-shortnames.conf" = {
-    enable = config.custom.files.".config/containers/registries.conf.d/000-shortnames.conf".enable;
-    executable = false;
-    text = ''
-      [aliases]
-        # almalinux
-        "almalinux" = "docker.io/library/almalinux"
-        "almalinux-minimal" = "docker.io/library/almalinux-minimal"
-        # containers
-        "skopeo" = "quay.io/skopeo/stable"
-        "podman" = "quay.io/podman/stable"
-        # docker
-        "alpine" = "docker.io/library/alpine"
-        "docker" = "docker.io/library/docker"
-        "registry" = "docker.io/library/registry"
-        # Fedora
-        "fedora-minimal" = "registry.fedoraproject.org/fedora-minimal"
-        "fedora" = "registry.fedoraproject.org/fedora"
-        # openSUSE
-        "opensuse/tumbleweed" = "registry.opensuse.org/opensuse/tumbleweed"
-        "tumbleweed" = "registry.opensuse.org/opensuse/tumbleweed"
-        # SUSE
-        "suse/sle15" = "registry.suse.com/suse/sle15"
-        "sle15" = "registry.suse.com/suse/sle15"
-        # Red Hat Enterprise Linux
-        "rhel" = "registry.access.redhat.com/rhel"
-        "rhel6" = "registry.access.redhat.com/rhel6"
-        "rhel7" = "registry.access.redhat.com/rhel7"
-        "ubi7" = "registry.access.redhat.com/ubi7"
-        "ubi7-init" = "registry.access.redhat.com/ubi7-init"
-        "ubi7-minimal" = "registry.access.redhat.com/ubi7-minimal"
-        "ubi8" = "registry.access.redhat.com/ubi8"
-        "ubi8-minimal" = "registry.access.redhat.com/ubi8-minimal"
-        "ubi8-init" = "registry.access.redhat.com/ubi8-init"
-        "ubi8-micro" = "registry.access.redhat.com/ubi8-micro"
-        "ubi8/ubi" = "registry.access.redhat.com/ubi8/ubi"
-        "ubi8/ubi-minimal" = "registry.access.redhat.com/ubi8-minimal"
-        "ubi8/ubi-init" = "registry.access.redhat.com/ubi8-init"
-        "ubi8/ubi-micro" = "registry.access.redhat.com/ubi8-micro"
-        # Debian
-        "debian" = "docker.io/library/debian"
-        # Ubuntu
-        "ubuntu" = "docker.io/library/ubuntu"
-        # Oracle Linux
-        "oraclelinux" = "container-registry.oracle.com/os/oraclelinux"
-        # busybox
-        "busybox" = "docker.io/library/busybox"
-        # php
-        "php" = "docker.io/library/php"
-        # python
-        "python" = "docker.io/library/python"
-        # node
-        "node" = "docker.io/library/node"
-    '';
+  options.custom.files = {
+    ".config/distrobox/distrobox.conf".enable = lib.mkEnableOption(lib.mdDoc "distrobox.conf file auto-population");
+    ".config/distrobox".enable = lib.mkEnableOption(lib.mdDoc "distrobox pre-init and init hooks");
+    ".config/containers".enable = lib.mkEnableOption(lib.mdDoc "podman ubuntu-like registry and storage settings");
+    ".config/containers/registries.conf.d/000-shortnames.conf".enable = lib.mkEnableOption(lib.mdDoc "podman predefined list of public image aliases");
   };
 
   config = {
+    home.file.".config/distrobox/distrobox.conf" = {
+      enable = config.custom.files.".config/distrobox/distrobox.conf".enable;
+      executable = false;
+      text =
+        ''
+          # support the init hooks (see home.file.distrobox_preinithooks)
+          container_pre_init_hook="~/.config/distrobox/pre-init-hooks.sh"
+          # support the init hooks (see home.file.distrobox_inithooks)
+          container_init_hook="~/.config/distrobox/init-hooks.sh"
+        '';
+    };
+
+    # distrobox hooks to copy are host-name specific
+    home.file.".config/distrobox" = {
+      # 'source' must be set in the custom.nix!
+      #source = ./home_files/distrobox;
+
+      enable = config.custom.files.".config/distrobox".enable;
+      # keep the permissions from the files in the fleek folder
+      executable = null;
+      # Make each individual file a symlink in the copy rather than symlinking the
+      # whole directory. We put other home.file's here too, so we can't do the
+      # latter.
+      recursive = true;
+    };
+
+    # The basic settings for podman.
+    # Still requires uidmap to be installed manually from built-in package manager.
+    # Pulled from Ubuntu.
+    home.file.".config/containers" = {
+      enable = config.custom.files.".config/containers".enable;
+      executable = false;
+      # Make each individual file a symlink in the copy rather than symlinking the
+      # whole directory. We put other home.file's here too, so we can't do the
+      # latter.
+      recursive = true;
+      source = ./home_files/podman_config;
+    };
+
+    # set of pre-defined short name aliases for images via podman
+    home.file.".config/containers/registries.conf.d/000-shortnames.conf" = {
+      enable = config.custom.files.".config/containers/registries.conf.d/000-shortnames.conf".enable;
+      executable = false;
+      text = ''
+        [aliases]
+          # almalinux
+          "almalinux" = "docker.io/library/almalinux"
+          "almalinux-minimal" = "docker.io/library/almalinux-minimal"
+          # containers
+          "skopeo" = "quay.io/skopeo/stable"
+          "podman" = "quay.io/podman/stable"
+          # docker
+          "alpine" = "docker.io/library/alpine"
+          "docker" = "docker.io/library/docker"
+          "registry" = "docker.io/library/registry"
+          # Fedora
+          "fedora-minimal" = "registry.fedoraproject.org/fedora-minimal"
+          "fedora" = "registry.fedoraproject.org/fedora"
+          # openSUSE
+          "opensuse/tumbleweed" = "registry.opensuse.org/opensuse/tumbleweed"
+          "tumbleweed" = "registry.opensuse.org/opensuse/tumbleweed"
+          # SUSE
+          "suse/sle15" = "registry.suse.com/suse/sle15"
+          "sle15" = "registry.suse.com/suse/sle15"
+          # Red Hat Enterprise Linux
+          "rhel" = "registry.access.redhat.com/rhel"
+          "rhel6" = "registry.access.redhat.com/rhel6"
+          "rhel7" = "registry.access.redhat.com/rhel7"
+          "ubi7" = "registry.access.redhat.com/ubi7"
+          "ubi7-init" = "registry.access.redhat.com/ubi7-init"
+          "ubi7-minimal" = "registry.access.redhat.com/ubi7-minimal"
+          "ubi8" = "registry.access.redhat.com/ubi8"
+          "ubi8-minimal" = "registry.access.redhat.com/ubi8-minimal"
+          "ubi8-init" = "registry.access.redhat.com/ubi8-init"
+          "ubi8-micro" = "registry.access.redhat.com/ubi8-micro"
+          "ubi8/ubi" = "registry.access.redhat.com/ubi8/ubi"
+          "ubi8/ubi-minimal" = "registry.access.redhat.com/ubi8-minimal"
+          "ubi8/ubi-init" = "registry.access.redhat.com/ubi8-init"
+          "ubi8/ubi-micro" = "registry.access.redhat.com/ubi8-micro"
+          # Debian
+          "debian" = "docker.io/library/debian"
+          # Ubuntu
+          "ubuntu" = "docker.io/library/ubuntu"
+          # Oracle Linux
+          "oraclelinux" = "container-registry.oracle.com/os/oraclelinux"
+          # busybox
+          "busybox" = "docker.io/library/busybox"
+          # php
+          "php" = "docker.io/library/php"
+          # python
+          "python" = "docker.io/library/python"
+          # node
+          "node" = "docker.io/library/node"
+      '';
+    };
+
     #####################################
     # Program configs
     #####################################
