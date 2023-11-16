8088c18494ac1b314fb2b05416cebc1ad443eb57
Alexander, Michael <michael.alexander@karlstorz.com>
2023-11-16 13:05:04 -0500

    fleek: apply

--- fedora/custom.nix
+++ fedora/custom.nix
@@ -1,80 +1,31 @@
 { pkgs, misc, ... }: {
   # FEEL FREE TO EDIT: This file is NOT managed by fleek. 
 
-  #####################################
-  # Programs
-  #####################################
+  imports = [
+    ../modules/fedora_shells.nix
+  ];
 
-  programs.bash.initExtra = ''
-    ######################################################################
-    # Fedora default is to source /etc/bashrc, but this includes unconditional
-    # overwrites of aliases and other undesirable things, so we reconstruct it
-    # manually instead.
-    ## Source global definitions
-    #if [ -f /etc/bashrc ]; then
-    #        . /etc/bashrc
-    #fi
+  # declare it explicitly so we can access the config.custom.files section to set options as well
+  config = {
+    # extra packages that should be installed only on this host
+    home.packages = [
+      pkgs.distrobox
+    ];
 
-    # Nix
-    if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
-        . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
-    fi
-    # End Nix
+    #####################################
+    # Files (arbitrary)
+    #####################################
 
-    # Prevent doublesourcing
-    if [ -z "$BASHRCSOURCED" ]; then
-      BASHRCSOURCED="Y"
+    # The primary distrobox config file
+    custom.distrobox.hooks = {
+      enable = true;
+      docker_sock = true;
+    };
 
-      if ! shopt -q login_shell ; then # We're not a login shell
-        # Need to redefine pathmunge, it gets undefined at the end of /etc/profile
-        pathmunge () {
-          case ":$${PATH}:" in
-            *:"$1":*)
-              ;;
-            *)
-              if [ "$2" = "after" ] ; then
-                PATH=$PATH:$1
-              else
-                PATH=$1:$PATH
-              fi
-          esac
-        }
-
-        # Set default umask for non-login shell only if it is set to 0
-        [ `umask` -eq 0 ] && umask 022
-
-        SHELL=/bin/bash
-        # Only display echos from profile.d scripts if we are no login shell
-        # and interactive - otherwise just process them to set envvars
-        for i in /etc/profile.d/{bash_completion,colorgrep,colorxzgrep,colorzgrep,debuginfod,flatpak,gawk,lang,less,nix,PackageKit,toolbox,vte,which2}.sh; do
-            if [ -r "$i" ]; then
-                if [ "$PS1" ]; then
-                    . "$i"
-                else
-                    . "$i" >/dev/null
-                fi
-            fi
-        done
-
-        unset i
-        unset -f pathmunge
-      fi
-    fi
-
-    # User specific environment
-    if ! [[ "$PATH" =~ "$HOME/.local/bin:$HOME/bin:" ]]
-    then
-        PATH="$HOME/.local/bin:$HOME/bin:$PATH"
-    fi
-    export PATH
-
-    # Uncomment the following line if you don't like systemctl's auto-paging feature:
-    # export SYSTEMD_PAGER=
-
-    unset rc
-    ######################################################################
-  '';
-
+    #####################################
+    # Programs
+    #####################################
+  };
 }
 
 # vim:sw=2:expandtab
--- goln-5cl17g3/custom.nix
+++ goln-5cl17g3/custom.nix
@@ -46,17 +46,11 @@
       if [ -e "$HOME/DevTools/.bashrc" ] ; then
         source $HOME/DevTools/.bashrc
       fi
-      if [ -e "$HOME/.jfrog/jfrog_bash_completion" ] ; then
-        source $HOME/.jfrog/jfrog_bash_completion
-      fi
     '';
     programs.zsh.initExtra = ''
 
       if [ -e "$HOME/.cargo/env" ] ; then
         source $HOME/.cargo/env
-      fi
-      if [ -e "$HOME/.jfrog/jfrog_bash_completion" ] ; then
-        source $HOME/.jfrog/jfrog_bash_completion
       fi
     '';
   };
