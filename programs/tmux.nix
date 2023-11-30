ab2bb7ea39bb3429846506934d27d416f07419e8
Alexander, Michael <michael.alexander@karlstorz.com>
2023-11-30 15:13:32 -0500

    fleek: apply

--- programs/tmux.nix
+++ programs/tmux.nix
@@ -8,14 +8,16 @@
     keyMode = "emacs";
     mouse = true;
 
-    # Allow tmux layouts, startup/exit behaviors, etc to be pre-defined in YAML.
-    # https://github.com/tmuxinator/tmuxinator
-    # Stores the settings (with --local) in .tmuxinator.yaml in the current folder
-    # Create a new project (multiple allowed per file)
-    #   tmuxinator new --local [project]
-    # Start a tmux session from the project:
-    #   tmuxinator start --local [project]
+    # currently conflicts with zsh-completions package, which has conflicting file permissions on the _tmuxinator completions file
+    ## Allow tmux layouts, startup/exit behaviors, etc to be pre-defined in YAML.
+    ## https://github.com/tmuxinator/tmuxinator
+    ## Stores the settings (with --local) in .tmuxinator.yaml in the current folder
+    ## Create a new project (multiple allowed per file)
+    ##   tmuxinator new --local [project]
+    ## Start a tmux session from the project:
+    ##   tmuxinator start --local [project]
     #tmuxinator.enable = true;
+    tmuxp.enable = true;
 
     #plugins = with pkgs; [
     #  # tmux-sensible always included automatically
