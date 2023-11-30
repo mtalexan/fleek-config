bc375a15e25839f273e3363a74d5d8a22d3913a8
Alexander, Michael <michael.alexander@karlstorz.com>
2023-11-30 14:41:17 -0500

    fleek: apply

--- user.nix
+++ user.nix
@@ -4,7 +4,26 @@
   # split up into separate files in the modules folder
   imports = [
     ./modules/files.nix
-    ./modules/programs.nix
+
+    ./programs/atuin.nix
+    ./programs/bat.nix
+    ./programs/dircolors.nix
+    ./programs/eza.nix
+    ./programs/fzf.nix
+    ./programs/git.nix
+    ./programs/jq.nix
+    ./programs/less.nix
+    ./programs/man.nix
+    ./programs/neovim.nix
+    ./programs/nix-index.nix
+    ./programs/noti.nix
+    ./programs/ripgrep.nix
+    ./programs/script-directory.nix
+    ./programs/tealdear.nix
+    ./programs/terminator.nix
+    ./programs/tmux.nix
+    ./programs/zoxide.nix
+
     ./modules/prompt.nix
     ./modules/shells.nix
   ];
