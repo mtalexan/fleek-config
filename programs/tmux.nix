bfd6e0d0ae42f43ed59bbf1ec3130a71b999e206
Alexander, Michael <michael.alexander@karlstorz.com>
2023-11-30 15:06:03 -0500

    fleek: apply

--- programs/tmux.nix
+++ programs/tmux.nix
@@ -1,10 +1,26 @@
 { pkgs, misc, lib, ... }: {
   programs.tmux = {
     enable = true;
+    # automatically includes the tmux-sensible plugin, which adapts some common options
+    # https://github.com/tmux-plugins/tmux-sensible
+
     historyLimit = 10000000;
     keyMode = "emacs";
     mouse = true;
 
+    # Allow tmux layouts, startup/exit behaviors, etc to be pre-defined in YAML.
+    # https://github.com/tmuxinator/tmuxinator
+    # Stores the settings (with --local) in .tmuxinator.yaml in the current folder
+    # Create a new project (multiple allowed per file)
+    #   tmuxinator new --local [project]
+    # Start a tmux session from the project:
+    #   tmuxinator start --local [project]
+    tmuxinator.enable = true;
+
+    #plugins = with pkgs; [
+    #  # tmux-sensible always included automatically
+    #  tmuxPlugins.
+    #];
   };
 }
 
