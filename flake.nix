632fef76913243607d87b345756c18120382a5b9
Mike <github@trackit.fe80.email>
2024-01-01 20:17:27 -0500

    fleek: apply

--- flake.nix
+++ flake.nix
@@ -182,6 +182,32 @@
         ];
       };
       
+      "mike@cloud-t610" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+        extraSpecialArgs = { inherit inputs; }; # Pass flake inputs to our config
+        modules = [
+          ./home.nix 
+          ./path.nix
+          ./shell.nix
+          ./user.nix
+          ./aliases.nix
+          ./programs.nix
+          # Host Specific configs
+          ./cloud-t610/mike.nix
+          ./cloud-t610/custom.nix
+          # self-manage fleek
+          {
+            home.packages = [
+              fleek.packages.x86_64-linux.default
+            ];
+          }
+          ({
+           nixpkgs.overlays = [];
+          })
+
+        ];
+      };
+      
     };
   };
 }
