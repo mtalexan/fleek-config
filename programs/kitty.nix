753d9f08358c570e70610d366c037680837bef9c
Alexander, Michael <michael.alexander@karlstorz.com>
2024-01-02 18:20:03 -0500

    fleek: apply

--- programs/kitty.nix
+++ programs/kitty.nix
@@ -22,7 +22,8 @@
 
   programs.kitty = {
     # can't install from nix
-    enable = false;
+    enable = true;
+    package = null;
     shellIntegration = {
       # Since automatic shell integration doesn't work in subshells, multiplexers, etc, we have to manually detect and load the code ourselves
       # as part of the rc file
