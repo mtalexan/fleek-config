327fa568c9f76c0b34b22e08e09e55c03ee1ea66
Mike <github@trackit.fe80.email>
2024-01-02 22:17:17 -0500

    fleek: apply

--- modules/shells.nix
+++ modules/shells.nix
@@ -116,7 +116,7 @@
           owner = "zsh-users";
           repo = "zsh-autosuggestions";
           rev = "v0.7.0";
-          sha256 = "sha256-000000000000000000000000000000000000000000=";
+          sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
         };
       }
       {
