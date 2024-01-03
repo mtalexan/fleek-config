caf2638cb9c159f10230ff8af06c009161db9cb1
Alexander, Michael <michael.alexander@karlstorz.com>
2024-01-03 12:50:38 -0500

    fleek: apply

--- programs/kitty.nix
+++ programs/kitty.nix
@@ -73,7 +73,7 @@
 
     extraConfig = lib.concatLines [
       ''
-        enabled_layouts Splits,Tall,Horizontal,Grid,Stack
+        enabled_layouts Splits
         map kitty_mod+enter>h launch --type=window --location=vsplit
         map kitty_mod+enter>v launch --type=window --location=hsplit
       ''
