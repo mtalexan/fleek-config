3c795225e5f6d87a57746bf3e0a1bffdaf1f9546
Alexander, Michael <michael.alexander@karlstorz.com>
2024-01-17 17:37:59 -0500

    fleek: apply

--- programs/homebrew.nix
+++ programs/homebrew.nix
@@ -5,16 +5,44 @@
   #   - 'brew install gcc'
   #
   # WARNING: homebrew should be installed before this is included in the custom.nix file
+  #          https://brew.sh/
 
-  programs.zsh.initExtra = lib.concatLines [
-    ''
-    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-    ''
-  ];
+  programs.zsh = {
+    initExtraBeforeCompInit = lib.concatLines [
+      ''
+      if command -v brew &>/dev/null; then
+        FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
+      fi
+      ''
+    ];
+    initExtra = lib.concatLines [
+      ''
+      if command -v brew &>/dev/null; then
+        eval "$($(brew --prefix)/bin/brew shellenv)"
+      fi
+      ''
+    ];
+  };
 
   programs.bash.initExtra = lib.concatLines [
     ''
-    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+    if command -v brew &>/dev/null; then
+      homebrew_prefix="$(brew --prefix)"
+
+      eval "$($homebrew_prefix/bin/brew shellenv)"
+
+      # add completions
+
+      # use /etc/profile.d/bash_completions.sh if it exists, otherwise 
+      # use each file from /etc/bash_completions.d/*
+      if [[ -r "$homebrew_prefix/etc/profile.d/bash_completions.sh" ]]; then
+        source "$homebrew_prefix/etc/profile.d/bash_completions.sh"
+      else
+        for C in "$homebrew_prefix/etc/bash_completions.d/"* ; do
+          [[ -r "$C" ]] && source "$C"
+        done
+      fi
+    fi
     ''
   ];
 }
