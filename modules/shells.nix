f36631d5cdc584d7b671988aa6c466dfc9db66bb
Mike <github@trackit.fe80.email>
2024-01-26 10:22:16 -0500

    fleek: apply

--- modules/shells.nix
+++ modules/shells.nix
@@ -144,7 +144,7 @@
           repo = "fast-syntax-highlighting";
           # post v1.55, there is no v1.56 yet
           rev = "cf318e06a9b7c9f2219d78f41b46fa6e06011fd9";
-          sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+          sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
         };
       }
     ];
