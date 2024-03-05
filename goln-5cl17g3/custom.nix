36027cd49730ade6f95774afc62a20a458689a0a
Alexander, Michael <michael.alexander@karlstorz.com>
2024-03-05 08:54:46 -0500

    fleek: apply

--- goln-5cl17g3/custom.nix
+++ goln-5cl17g3/custom.nix
@@ -45,6 +45,16 @@
       "$HOME/DevTools/bin"
     ];
 
+    home.sessionVariables = {
+      # use the system certs for Node clients like VSCode
+      NODE_EXTRA_CA_CERTS = "/etc/ssl/certs/ca-certificates.crt";
+      # use the system certs for Python code using the Requests module
+      REQUESTS_CA_BUNDLE = "/etc/ssl/certs/ca-certificates.crt";
+      # use the system certs for other Python code
+      SSL_CERT_FILE = "/etc/ssl/certs/ca-certificates.crt";
+
+    };
+
     programs.bash.initExtra = lib.concatLines [
       ''
       if [ -e "$HOME/DevTools/.bashrc" ] ; then
