aa59652f23b9ad17fa50ea076af4c28788a946ba
Alexander, Michael <michael.alexader@karlstorz.com>
2024-03-06 11:19:26 -0500

    fleek: new system

---
+++ vm-gol-422Q533/dev.nix
@@ -0,0 +1,29 @@
+{ pkgs, misc, ... }: {
+  # DO NOT EDIT: This file is managed by fleek. Manual changes will be overwritten.
+    home.username = "dev";
+    home.homeDirectory = "/home/dev";
+    programs.git = {
+        enable = true;
+        aliases = {
+            pushall = "!git remote | xargs -L1 git push --all";
+            graph = "log --decorate --oneline --graph";
+            add-nowhitespace = "!git diff -U0 -w --no-color | git apply --cached --ignore-whitespace --unidiff-zero -";
+        };
+        userName = "Mike";
+        userEmail = "github@trackit.fe80.email";
+        extraConfig = {
+            feature.manyFiles = true;
+            init.defaultBranch = "main";
+            gpg.format = "ssh";
+        };
+
+        signing = {
+            key = "~/.ssh/github_personal_ed25519";
+            signByDefault = builtins.stringLength "~/.ssh/github_personal_ed25519" > 0;
+        };
+
+        lfs.enable = true;
+        ignores = [ ".direnv" "result" ];
+  };
+  
+}
