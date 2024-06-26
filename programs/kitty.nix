ed4cf93c47ebac302993ba8ec34cb07b648db1d3
Mike <github@trackit.fe80.email>
2024-06-26 12:33:53 -0400

    fleek: apply

--- programs/kitty.nix
+++ programs/kitty.nix
@@ -1490,6 +1490,7 @@
         #: Advanced {{{
 
         # shell .
+        shell zsh
 
         #: The shell program to execute. The default value of . means to use
         #: whatever shell is set as the default shell for the current user.
