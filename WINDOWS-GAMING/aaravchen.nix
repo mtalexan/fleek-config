e82cb7ff05c7e17fc348e9661246226a1219a8f0
mtalexan <github@trackit.fe80.email>
2024-07-05 00:48:13 -0400

    fleek: new system

---
+++ WINDOWS-GAMING/aaravchen.nix
@@ -0,0 +1,29 @@
+{ pkgs, misc, ... }: {
+  # DO NOT EDIT: This file is managed by fleek. Manual changes will be overwritten.
+    home.username = "aaravchen";
+    home.homeDirectory = "/home/aaravchen";
+    programs.git = {
+        enable = true;
+        aliases = {
+            pushall = "!git remote | xargs -L1 git push --all";
+            graph = "log --decorate --oneline --graph";
+            add-nowhitespace = "!git diff -U0 -w --no-color | git apply --cached --ignore-whitespace --unidiff-zero -";
+        };
+        userName = "Mike";
+        userEmail = "github@trackit.fe80.email";
+        extraConfig = {
+            feature.manyFiles = true;
+            init.defaultBranch = "main";
+            gpg.format = "ssh";
+        };
+
+        signing = {
+            key = "~/.ssh/id_ed25519_github";
+            signByDefault = builtins.stringLength "~/.ssh/id_ed25519_github" > 0;
+        };
+
+        lfs.enable = true;
+        ignores = [ ".direnv" "result" ];
+  };
+  
+}
