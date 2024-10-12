6795d9632cd5f1270ebd07f7ea55d92580f1f414
mtalexan <github@trackit.fe80.email>
2024-10-11 21:28:56 -0400

    fleek: apply

--- bazzite/custom.nix
+++ bazzite/custom.nix
@@ -1,5 +1,41 @@
 { pkgs, misc, ... }: {
   # FEEL FREE TO EDIT: This file is NOT managed by fleek. 
 
- 
+  imports = [
+    #../modules/fedora_shells.nix
+    ../programs/terminator.nix
+    ../programs/kitty.nix
+  ];
+
+  # declare it explicitly so we can access the config.custom.files section to set options as well
+  config = {
+    # extra packages that should be installed only on this host
+    home.packages = [
+      pkgs.distrobox
+    ];
+
+    #####################################
+    # Files (arbitrary)
+    #####################################
+
+    # The primary distrobox config file
+    custom.distrobox.hooks = {
+      enable = true;
+      docker_sock = true;
+    };
+
+    #####################################
+    # Programs
+    #####################################
+
+    home.shellAliases = {
+      # end all aliases in a space so completion on arguments works for them
+
+      # Flatpaks
+      "meld" = "flatpak run org.gnome.meld ";
+    };
+  };
 }
+
+
+# vim:sw=2:expandtab
