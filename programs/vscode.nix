166dd477f2d09e0e3033d5d140bbb3e00f7979e2
Alexander, Michael <michael.alexander@karlstorz.com>
2024-11-27 09:26:22 -0500

    fleek: apply

--- programs/vscode.nix
+++ programs/vscode.nix
@@ -1,6 +1,8 @@
 { pkgs, misc, lib, ... }: {
   # already includes it's own *.desktop entry file, you just have to restart the gnome session to get it to show up
 
+  # Do NOT use the home-manager settings. It installs its own config that prevents the settings from being synced or modified in the GUI.
+  # Instead, install only the package.  This still has the program and the desktop files, but doesn't try to manage the settings files.
   home.packages = [
     pkgs.vscode
   ];
