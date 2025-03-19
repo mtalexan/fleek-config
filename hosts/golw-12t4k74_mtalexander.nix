32da673263de82719c618630075199508f78c408
mtalexan <github@trackit.fe80.email>
2025-03-18 18:51:18 -0600

    Convert golw-12t4k74 host to it's own file and disable gpu

--- hosts/golw-12t4k74_mtalexander.nix
+++ hosts/golw-12t4k74_mtalexander.nix
@@ -1,1 +1,53 @@
-goln-5cl17g3_mtalexander.nix
+{ pkgs, misc, lib, config, ... }: {
+
+  imports = [
+    ../identities/ks.nix # set the default git identity
+    ../programs/ks-dev-tools.nix
+    ../programs/kitty.nix
+    ../programs/distrobox.nix
+    ../programs/vscode.nix
+  ];
+
+  # declare it explicitly so we can access the config.custom.files section to set options as well
+  config = {
+    # Host-specific username and home location
+    home.username = "mtalexander";
+    home.homeDirectory = "/home/mtalexander";
+    # TODO: Set this to point to the work SSH key 
+    #programs.git.signing.key = "~/.ssh/github_personal_ed25519";
+
+    #####################################
+    # Extra host-unique non-configurable packages
+    #####################################
+
+    home.packages = [
+      pkgs.rename
+      # don't use podman or skopeo from nix,
+      # podman is suddenly experiencing a bug where 'podman run --userns:keep-id ...' isn't properly linking
+      #   the overlay folders together and fails to start any containers.
+      # skopeo wasn't built with glibc-static and CGO, so it can't parse users or groups from LDAP.
+    ];
+
+    #####################################
+    # Custom defined config settings
+    #####################################
+    custom = {
+      nixGL.gpu = false;
+
+      distrobox = {
+        hooks = {
+          enable = true;
+          host_certs = true;
+          docker_sock = true;
+        };
+        config.engine = "docker";
+      };
+    };
+
+    #####################################
+    # One-off Program Settings
+    #####################################
+  };
+}
+
+# vim: ts=2:sw=2:expandtab
