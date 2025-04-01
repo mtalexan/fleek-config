e440425ba00fd5effacb59fa2bab6da67db8dc1b
mtalexan <github@trackit.fe80.email>
2025-04-01 10:43:21 -0600

    Move atuin sync settings to identify file

--- hosts/golw-12t4k74_mtalexander.nix
+++ hosts/golw-12t4k74_mtalexander.nix
@@ -15,14 +15,6 @@
     home.homeDirectory = "/home/mtalexander";
     # the location on this specific machine where the default SSH  key is to use for signing
     programs.git.signing.key = "~/.ssh/gitlab_ed25519";
-    programs.atuin.settings = {
-      # enable syncing using the E2EE default server
-      #auto_sync = true;
-      # sync after every command
-      sync_frequency = "0";
-      #key_path = ;
-      #session_path = ;
-    }
 
 
     #####################################
