5afe21b99e70e3ffa0bfb121fb789b0fef364a2a
mtalexan <github@trackit.fe80.email>
2025-04-01 15:17:29 -0600

    homeage init config

--- programs/homeage.nix
+++ programs/homeage.nix
@@ -2,11 +2,11 @@
   # WARNING: because we need an 'options.' key here, we have to use the verbose format with a 'config.home.' for home-manager things
 
   # Not configurable in home-manager itself, add the package generically and setup config.home.file's for what we need
-  config.home.packages = [
+  home.packages = [
     pkgs.ragenix
   ];
 
-  config.homeage = {
+  inputs.homeage = {
     # this is the (r)agenix package, not a homeage package. Make sure it's listed above
     pkg = pkgs.ragenix;
     # Where the decrypted secrets are mounted. Defaults to /run/user/$UID/secrets/
