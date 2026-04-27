ef6c0d79695c031d9d7d6f5e885375f3769527e3
Mike <github@trackit.fe80.email>
2026-04-27 13:46:07 -0600

    Forgot the sha update for the newer nvidia version

--- hosts/goln-5wwdx54_mtalexander.nix
+++ hosts/goln-5wwdx54_mtalexander.nix
@@ -42,7 +42,7 @@
     targets.genericLinux.gpu.nvidia = {
       enable = true;
       version = "580.126.09";
-      sha256 = "sha256-hJ7w746EK5gGss3p8RwTA9VPGpp2lGfk5dlhsv4Rgqc=";
+      sha256 = "sha256-TKxT5I+K3/Zh1HyHiO0kBZokjJ/YCYzq/QiKSYmG7CY=";
     };
     
     #####################################
