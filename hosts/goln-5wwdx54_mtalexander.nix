09d01a4433157e628b610f437f1882c9154624f7
Mike <github@trackit.fe80.email>
2026-07-21 16:28:01 -0700

    Normalize hostnames to lowercase before using them, and rename the one file that didn't match. Update the NVIDIA version in goln-55wdx54

--- hosts/goln-5wwdx54_mtalexander.nix
+++ hosts/goln-5wwdx54_mtalexander.nix
@@ -41,8 +41,8 @@
     #
     targets.genericLinux.gpu.nvidia = {
       enable = true;
-      version = "580.142"; # this particular version was only 2-part SemVer for some reason
-      sha256 = "sha256-IJFfzz/+icNVDPk7YKBKKFRTFQ2S4kaOGRGkNiBEdWM=";
+      version = "595.71.05";
+      sha256 = "sha256-NiA7iWC35JyKQva6H1hjzeNKBek9KyS3mK8G3YRva4I=";
     };
     
     #####################################
